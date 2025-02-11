dfa26acc532b09158f2bb7ef16a9eced03a4d4eb
UnicornOnAzur <unicorn.on.azur@gmail.com>
2025-02-11 08:19:49 +0100

    Added numpy and refactored

--- src/public_repository/code.py
+++ src/public_repository/code.py
@@ -1,9 +1,12 @@
+import numpy as np
 import pandas as pd
 import plotly.express as px
+
 
 def make_dataframe(col):
     df = pd.DataFrame(columns=["a", "b", "c"])
     col.dataframe(data=df, use_container_width=True)
+
 
 def make_graph(col):
     df = px.data.gapminder().query("year == 2007")
@@ -12,6 +15,7 @@ def make_graph(col):
                     color_continuous_scale='RdBu',
                     color_continuous_midpoint=np.average(df['lifeExp'], weights=df['pop']))
     col.plotly_graph(fig)
+
 
 if __name__ == "__main__":
     print("This is the open repository")
