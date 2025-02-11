56bcab76e97c874a55fc4fdafff7d9fd8c8cfd7e
UnicornOnAzur <unicorn.on.azur@gmail.com>
2025-02-11 08:21:46 +0100

    Added correct streamlit function

--- src/public_repository/code.py
+++ src/public_repository/code.py
@@ -11,10 +11,10 @@ def make_dataframe(col):
 def make_graph(col):
     df = px.data.gapminder().query("year == 2007")
     fig = px.treemap(df, path=[px.Constant("world"), 'continent', 'country'], values='pop',
-                    color='lifeExp', hover_data=['iso_alpha'],
-                    color_continuous_scale='RdBu',
-                    color_continuous_midpoint=np.average(df['lifeExp'], weights=df['pop']))
-    col.plotly_graph(fig)
+                     color='lifeExp', hover_data=['iso_alpha'],
+                     color_continuous_scale='RdBu',
+                     color_continuous_midpoint=np.average(df['lifeExp'], weights=df['pop']))
+    col.plotly_chart(fig)
 
 
 if __name__ == "__main__":
