bb6c6638f5c4d82270507f672979a72afdb4c60f
UnicornOnAzur <unicorn.on.azur@gmail.com>
2025-02-11 08:23:57 +0100

    Added key

--- src/public_repository/code.py
+++ src/public_repository/code.py
@@ -13,7 +13,7 @@ def make_graph(col):
     fig = px.treemap(df, path=[px.Constant("world"), 'continent', 'country'], values='pop',
                      color='lifeExp', hover_data=['iso_alpha'],
                      color_continuous_scale='RdBu',
-                     color_continuous_midpoint=np.average(df['lifeExp'], weights=df['pop']))
+                     color_continuous_midpoint=np.average(df['lifeExp'], weights=df['pop']), key="public")
     col.plotly_chart(fig)
 
 
